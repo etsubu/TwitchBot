3848a6590139dc0c8f70d164f1e3d65bd61b8304
Spanfile <spansdev@gmail.com>
2018-02-13 20:35:20 +0200

    Lock the permission dictionary when accessing in PermissionManager

--- TwitchBot/Commands/Permissions/PermissionManager.cs
+++ TwitchBot/Commands/Permissions/PermissionManager.cs
@@ -16,16 +16,22 @@ namespace TwitchBot.Commands.Permissions
         public int QueryPermission(string channel, string username)
         {
             var pair = new ChannelUsernamePair(channel, username);
-            return permissions.TryGetValue(pair, out var permission) ? permission : 0;
+            lock (permissions)
+            {
+                return permissions.TryGetValue(pair, out var permission) ? permission : 0;
+            }
         }
 
         public void UpdatePermission(string channel, string username, int permission)
         {
             var pair = new ChannelUsernamePair(channel, username);
-            if (permissions.ContainsKey(pair))
-                permissions[pair] = permission;
-            else
-                permissions.Add(pair, permission);
+            lock (permissions)
+            {
+                if (permissions.ContainsKey(pair))
+                    permissions[pair] = permission;
+                else
+                    permissions.Add(pair, permission);
+            }
         }
     }
 }
